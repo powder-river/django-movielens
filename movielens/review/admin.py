4e9859969e83aca61c709733a88a5b250759ff16
Danai Adkisson <dadkisso@live.com>
2015-10-06 14:42:28 -0400

    add admin interface for migrations

--- movielens/review/admin.py
+++ movielens/review/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Movie, Rater, Rating
 
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+class RaterAdmin(admin.ModelAdmin):
+    list_dispay = ['age']
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display =['movie','rating','rater']
 # Register your models here.
+
+
+admin.site.register(Movie,MovieAdmin)
+admin.site.register(Rater,RaterAdmin)
+admin.site.register(Rating,RatingAdmin)
