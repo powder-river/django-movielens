45793d370b0d42c1f0604bd642c45b23d955b853
Danai Adkisson <dadkisso@live.com>
2015-10-07 15:22:43 -0400

    read in movie lens user data

--- movielens/review/models.py
+++ movielens/review/models.py
@@ -9,6 +9,9 @@ class Movie(models.Model):
         return self.title
 class Rater(models.Model):
     age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=5)
+    occupation = models.CharField(max_length=100)
 
     def __str__(self):
         return str(self.id)
@@ -19,3 +22,32 @@ class Rating(models.Model):
     rating = models.IntegerField()
     movie = models.ForeignKey(Movie)
     rater = models.ForeignKey(Rater)
+
+
+def load_ml_data():
+    import csv
+    import json
+    import re
+
+    users = []
+    with open('ml-1m/users.dat') as f:
+        reader = csv.DictReader([line.replace('::', '\t') for line in f],
+                                fieldnames='UserID::Gender::Age::Occupation::Zip-code'.split('::'),
+                                delimiter='\t')
+        for row in reader:
+            user = {
+                'fields': {
+                    'age': row['Age'],
+                    'gender': row['Gender'],
+
+                    'occupation': row['Occupation'],
+                    'zipcode': row['Zip-code'],
+                },
+                'model': 'review.Rater',
+                'pk': int(row['UserID']),
+            }
+        users.append(user)
+
+
+    with open('users.json', 'w') as f:
+        f.write(json.dumps(users))
