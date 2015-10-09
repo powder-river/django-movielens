30127f78f25690d8ab128e8ff6552f8e6ee4f7a5
Danai Adkisson <dadkisso@live.com>
2015-10-09 13:53:39 -0400

    update templates and urls

--- movielens/review/urls.py
+++ movielens/review/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     url(r'^movies/all', views.all_movies, name='movie_list'),
     url(r'^raters/all', views.rater_detail, name='rater_list'),
     url(r'^raters/(?P<rater_id>\d+)$', views.show_rater, name='rater_detail')
-    # url(r'movie/(?P<movie_id>\d+)$', views.movie_detail)
-    # url(r'^statuses/', views.recent_statuses),
-    # url(r'^user/(?P<user_id>\d+)$', views.show_user),
-    # url(r'^user/(?P<username>\S+)$', views.show_user_by_username),
 ]
